544eb77115bd173c07fe74471d377b4205787f08
SuranjanaSrinivasan <220701293@rajalakshmi.edu.in>
2025-10-28 18:43:18 +0530

    Update API endpoint for admin sign-in

--- src/pages/AdminSignIn.jsx
+++ src/pages/AdminSignIn.jsx
@@ -28,7 +28,7 @@ const AdminSignIn = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/admin/signin', {
+      const response = await fetch('https://cert-admin-webapp-24-fyekedh8evdyh0aw.centralindia-01.azurewebsites.net/api/auth/admin/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
